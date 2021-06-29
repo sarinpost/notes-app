0ca652146519946d552a441ff83d56de6d2b7f8a
sarinpost <sarin.chococrm@gmail.com>
2021-06-29 18:42:19 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,52 @@
+const chalk = require('chalk');
+const yargs = require('yargs')
+const notes = require('./notes.js')
+
+// customize yarge version
+yargs.version('1.1.0')
+
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body:',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: (argv) => {
+        notes.addNote(argv.title, argv.body)
+    }
+})
+
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a new note',
+    handler: () => {
+        console.log('Remove a note !')
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'Read a new note',
+    handler: () => {
+        console.log('Read note !')
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'List notes',
+    handler: () => {
+        console.log('List note !')
+    }
+})
+
+yargs.parse()
